1ca5cef6bfb53f653006e37088ce9c8e1685ab3e
ashaya sinha <ashaya.sinha@gmail.com>
2025-06-01 20:08:41 +0530

    updating sign in, sign up and login page

--- src/App.js
+++ src/App.js
@@ -1,11 +1,8 @@
-import './App.css';
+import Body from './components/Body';
 
 function App() {
-  return (
-    <div className= "font-bold text-2xl">
-      Hello everyone this is me
-    </div>
-  );
+  return <Body />;
+  
 }
 
 export default App;
